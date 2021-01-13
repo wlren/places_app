8fa23f491777c2ff70785c76eee0ea66da528283
Ren Weilin <weilinwork99@gmail.com>
2021-01-13 23:29:59 +0800

    Remove access to API key and update readme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # places_app
 
-A new Flutter project that uses google Maps api
+A new Flutter project that uses google Maps api.
+Add google api key in lib/helpers/location_helper.dart before building and testing
--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-const GOOGLE_API_KEY = 'AIzaSyBee-mx825MGVuy_b_uIwpUvOz-Ej8RUyA';
+const GOOGLE_API_KEY = "ADD YOUR GOOGLE API KEY HERE";
 
 class LocationHelper {
   static String generateLocationImage({double latitude, double longitude}) {
